05312f7cc2a66baee4f7e1d09cc6dd6d9b7e3cb3
Alex Pablo de Oliveira Moraes <oliveiramoraes230@gmail.com>
2025-04-06 17:05:27 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,86 @@
 <!-- src/App.vue -->
 <template>
-  <v-app flat color="transparent" class="bg-transparent">
-    <Header @search="searchMovies" />
-    
-    <v-main style="background-color: transparent;">
-      <v-container fluid style="background-color: transparent;">
+  <v-app>
+    <Header />
+
+    <v-main class="flex-grow-1">
+      <v-container fluid>
         <router-view />
       </v-container>
     </v-main>
-    
-    <v-footer app class="text-center">
-      <p class="ma-0">
-        © {{ new Date().getFullYear() }} CineInfo - Dados e imagens fornecidos por 
-        <a href="https://www.themoviedb.org/" target="_blank" class="text-decoration-none">TMDB</a>
-      </p>
-    </v-footer>
+
+    <Footer />
   </v-app>
 </template>
 
 <script>
-import Header from '@/components/Header.vue'
+import Header from '@/components/AppHeader.vue'
+import Footer from '@/components/AppFooter.vue'
 
-  
 export default {
   components: {
-    Header
-  },
-  methods: {
-    searchMovies(query) {
-      // Aqui você pode acionar uma busca global ou redirecionar pra uma tela com resultados
-      console.log('Buscando por:', query)
-    }
+    Header,
+    Footer
   }
 }
 </script>
 
 <style>
 /* Fonte global para toda a aplicação */
-html, body, .v-application{
+html,
+body {
   font-family: 'Inter', sans-serif !important;
-  background-image: url('@/assets/background.png');
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
+}
+
+/* Aplicando o background na v-app */
+.v-application {
+  font-family: 'Inter', sans-serif !important;
+  background-image: url('@/assets/background.png') !important;
+  background-size: cover !important;
+  background-position: center !important;
+  background-repeat: no-repeat !important;
+  background-attachment: fixed !important;
+}
+
+/* Deixar o app transparente para mostrar o background */
+.v-app {
+  background: transparent !important;
 }
 
 /* Estilos para o Header */
-[class^="text-"], .v-btn, .v-card, .v-alert, .v-toolbar, .v-app-bar {
+[class^="text-"],
+.v-btn,
+.v-card,
+.v-alert,
+.v-toolbar,
+.v-app-bar {
   font-family: 'Inter', sans-serif !important;
+}
+
+/* Estilos para o app */
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+/* Estilos para o v-main */
+.v-main {
+  flex: 1 0 auto;
+  display: flex;
+  flex-direction: column;
+  background: transparent !important;
+}
+
+/* Estilos para o conteúdo */
+.v-main>.v-container {
+  flex-grow: 1;
+  background: transparent !important;
+}
+
+/* Estilos para o footer */
+.v-footer {
+  background: transparent !important;
 }
 
 /* Estilos globais */
